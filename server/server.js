6fab25ae56371eae210f1a7976712222d021f5cd
Kylene Hohman <kylenehohman@gmail.com>
2021-12-09 20:41:08 -0500

    working webpack

--- server/server.js
+++ server/server.js
@@ -3,23 +3,14 @@ const app = express();
 const path = require('path');
 
 
-
-// if (process.env.NODE_ENV === 'production'){
-//     // statically serve everything in the build folder on the route '/build'
-//   app.use('/build', express.static(path.join(__dirname, '../build')));
-//   // serve index.html on the route '/'
-//   app.get('/', (req, res) => {
-//     return res.status(200).sendFile(path.join(__dirname, '../index.html'));
-//   });
-// }
-
+if (process.env.NODE_ENV === 'production'){
     // statically serve everything in the build folder on the route '/build'
   app.use('/build', express.static(path.join(__dirname, '../build')));
   // serve index.html on the route '/'
   app.get('/', (req, res) => {
-    return res.status(200).sendFile(path.join(__dirname, './src/index.html'));
+    return res.status(200).sendFile(path.join(__dirname, '../src/index.html'));
   });
-
+}
 
 
 app.listen(3000);
--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    // mode: process.env.NODE_ENV,
-    mode: 'development',
+    mode: process.env.NODE_ENV,
+    // mode: 'development',
 
     entry: './src/index.js',
 
@@ -24,21 +24,21 @@ module.exports = {
                     presets: ['@babel/preset-env', '@babel/preset-react']
                 },
             },
-            // {
-            //     test: /\.s?css/,
-            //     exclude: /node_modules/,
-            //     use: [
-            //       'style-loader', 'css-loader', 'sass-loader'
-            //     ],
-            //   },
             {
-                test: /\.scss$/,
+                test: /\.s?css/,
+                exclude: /node_modules/,
                 use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
+                  'style-loader', 'css-loader', 'sass-loader'
                 ],
-            }
+              },
+            // {
+            //     test: /\.scss$/,
+            //     use: [
+            //         MiniCssExtractPlugin.loader,
+            //         'css-loader',
+            //         'sass-loader',
+            //     ],
+            // }
         ]
 
     },
@@ -48,16 +48,16 @@ module.exports = {
             // title: 'development',
             template: './src/index.html'
         }),
-        new MiniCssExtractPlugin(),
+        // new MiniCssExtractPlugin(),
     ],
 
-    // devServer: {
-    //     static: {
-    //         publicPath: '/build',
-    //         directory: path.resolve(__dirname, 'build'),
-    //     },
-    //     proxy: {
-    //         '/api': 'http://localhost:3000'
-    //     }
-    // }
+    devServer: {
+        static: {
+            publicPath: '/build',
+            directory: path.resolve(__dirname, 'build'),
+        },
+        proxy: {
+            '/api': 'http://localhost:3000'
+        }
+    }
 };
