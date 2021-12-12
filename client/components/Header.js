380c00a419d4396106fbe528fdbf892d8a438c14
Kylene Hohman <kylenehohman@gmail.com>
2021-12-11 22:55:38 -0500

    updated header

--- client/components/Header.js
+++ client/components/Header.js
@@ -3,17 +3,19 @@ import Button from './Button';
 import headerimg from '../assets/headerimg.png';
 
 const Header = () => {
-    return (
-        <header className='header'>
+  return (
+    <header className='header'>
+      {/* <img src={headerimg} className='logo' alt='header img' /> */}
+      <nav className='nav'>
+        {/* <img src={headerimg} className='logo' alt='header img' /> */}
+        <a href='/'>HOME</a>
+        <a href='/'>WISHLIST</a>
+        <a href='/'>ABOUT</a>
+        {/* <Button text='Login' /> */}
         <img src={headerimg} className='logo' alt='header img' />
-        <nav className='nav'>
-            <a href='/'>Home</a>
-            <a href='/'>Wishlist</a>
-            <a href='/'>About</a>
-            <Button text='Login' />
-        </nav>
-        </header>
-    )
-}
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
